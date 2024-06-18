42456aee46a12437623ab1bb76e0a070a90489a7
Mathis LAVEILLE <mlaveille@groupe-sturno.LOCAL>
2024-06-18 15:11:24 +0200

    projet

--- django-web-app/app/home/home/settings.py
+++ django-web-app/app/home/home/settings.py
@@ -7,7 +7,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'mailhost.stgs.fr'
 EMAIL_PORT = 25
 EMAIL_USE_TLS = False
-EMAIL_HOST_USER = 'Mathis-LAVEILLE@sturno.fr'
+EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
 
 
